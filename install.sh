f958916a505a8897259a0904127aa165070072a3
War Torrente I <42901134+catx0rr@users.noreply.github.com>
2023-11-15 17:25:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-function clean_up() {
-    rm -rf $PWD/tools $PWD/nuclei-templates
-}
-
 function install_aquatone() {
     if [ ! -f "$PWD/aquatone" ]; then
 	curl https://api.github.com/repos/michenriksen/aquatone/releases \
@@ -11,48 +7,48 @@ function install_aquatone() {
 		| head -n1 \
 		| cut -d: -f2,3 \
 		| tr -d '"' \
-		| wget -qi - -P $PWD/tools/aquatone
+		| wget -qi - -P $PWD/tools/amd64/aquatone
 
-        unzip $PWD/tools/aquatone/aquatone_linux_amd64_*.*.*.zip -d $PWD/tools/aquatone
-        rm -rf $PWD/tools/aquatone/aquatone_linux_amd64_*.*.*.zip
+        unzip $PWD/tools/amd64/aquatone/aquatone_linux_amd64_*.*.*.zip -d $PWD/tools/amd64/aquatone
+        rm -rf $PWD/tools/amd64/aquatone/aquatone_linux_amd64_*.*.*.zip
     fi
-    
+
     if [[ `dpkg-query -l chromium | grep ii | awk '{print $2}'` != "chromium" ]]; then
         apt-get install -y chromium
     fi
 }
 
-function install_aquatone_arm64() {                                        
-    if [ ! -f "$PWD/aquatone" ]; then                                
-        curl https://api.github.com/repos/michenriksen/aquatone/releases \
-                | grep "browser_download_url.*_linux_arm64_*.*.*zip" \
-                | head -n1 \                                         
-                | cut -d: -f2,3 \
-		| tr -d '"' \                                        
-                | wget -qi - -P $PWD/tools/aquatone                  
-                                                                     
-        unzip $PWD/tools/aquatone/aquatone_linux_arm64_*.*.*.zip -d $PWD/tools/aquatone_arm64
-        rm -rf $PWD/tools/aquatone/aquatone_linux_arm64_*.*.*.zip    
-    fi                                                               
-                                                                     
+function install_aquatone_arm64() {
+    if [ ! -f "$PWD/aquatone" ]; then
+	curl https://api.github.com/repos/michenriksen/aquatone/releases \
+		| grep "browser_download_url.*_linux_arm64_*.*.*zip" \
+		| head -n1 \
+		| cut -d: -f2,3 \
+		| tr -d '"' \
+		| wget -qi - -P $PWD/tools/arm64/aquatone
+
+        unzip $PWD/tools/arm64/aquatone/aquatone_linux_arm64_*.*.*.zip -d $PWD/tools/arm64/aquatone
+        rm -rf $PWD/tools/arm64/aquatone/aquatone_linux_arm64_*.*.*.zip
+    fi
+
     if [[ `dpkg-query -l chromium | grep ii | awk '{print $2}'` != "chromium" ]]; then
         apt-get install -y chromium
-    fi                                                               
-}  
+    fi
+}
 
-function install_nuclei() {                                                                                                                
-    if [ ! -f "$PWD/nuclei" ]; then                                  
-        curl https://api.github.com/repos/projectdiscovery/nuclei/releases \
-                | grep "browser_download_url.*_linux_amd64.zip" \
-                | head -n1 \    
-                | cut -d: -f2,3 \                                    
-                | tr -d '"' \                                                                                                              
-                | wget -qi - -P $PWD/tools/nuclei          
+function install_nuclei() {
+    if [ ! -f "$PWD/nuclei" ]; then
+	curl https://api.github.com/repos/projectdiscovery/nuclei/releases \
+		| grep "browser_download_url.*_linux_amd64.zip" \
+		| head -n1 \
+		| cut -d: -f2,3 \
+		| tr -d '"' \
+		| wget -qi - -P $PWD/tools/amd64/nuclei
 
-        unzip $PWD/tools/nuclei/nuclei_*.*.*_linux_amd64.zip -d $PWD/tools/nuclei/
-        rm -rf $PWD/tools/nuclei/nuclei_*.*.*_linux_amd64.zip
-    fi                                                               
-} 
+        unzip $PWD/tools/amd64/nuclei/nuclei_*.*.*_linux_amd64.zip -d $PWD/tools/amd64/nuclei
+        rm -rf $PWD/tools/amd64/nuclei/nuclei_*.*.*_linux_amd64.zip
+    fi
+}
 
 function install_nuclei_arm64() {
     if [ ! -f "$PWD/nuclei" ]; then
@@ -61,10 +57,10 @@ function install_nuclei_arm64() {
 		| head -n1 \
 		| cut -d: -f2,3 \
 		| tr -d '"' \
-		| wget -qi - -P $PWD/tools/nuclei
+		| wget -qi - -P $PWD/tools/arm64/nuclei
 
-        unzip $PWD/tools/nuclei/nuclei_*.*.*_linux_arm64.zip -d $PWD/tools/nuclei_arm64/
-        rm -rf $PWD/tools/nuclei/nuclei_*.*.*_linux_arm64.zip
+        unzip $PWD/tools/arm64/nuclei/nuclei_*.*.*_linux_arm64.zip -d $PWD/tools/arm64/nuclei
+        rm -rf $PWD/tools/arm64/nuclei/nuclei_*.*.*_linux_arm64.zip
     fi
 }
 
@@ -75,23 +71,23 @@ function install_httpx() {
 		| head -n1 \
 		| cut -d: -f2,3 \
 		| sed 's/"//g' \
-		| wget -qi - -P $PWD/tools/httpx
-        unzip $PWD/tools/httpx/httpx_*.*.*_linux_amd64.zip -d $PWD/tools/httpx
-        rm -rf $PWD/tools/httpx/httpx_*.*.*_linux_amd64.zip
+		| wget -qi - -P $PWD/tools/amd64/httpx
+        unzip $PWD/tools/amd64/httpx/httpx_*.*.*_linux_amd64.zip -d $PWD/tools/amd64/httpx
+        rm -rf $PWD/tools/amd64/httpx/httpx_*.*.*_linux_amd64.zip
     fi
 }
 
-function install_httpx_arm64() {                                                                                                                 
-    if [ ! -f "$PWD/httpx" ]; then                                                                                                         
-        curl https://api.github.com/repos/projectdiscovery/httpx/releases \                                                                
-                | grep "browser_download_url.*_linux_arm64.zip" \                                                                          
-                | head -n1 \                                         
-                | cut -d: -f2,3 \                                    
-                | sed 's/"//g' \                                                                                                                                                                                                                                                      
-                | wget -qi - -P $PWD/tools/httpx                                                                                                                                                                                                                                   
-        unzip $PWD/tools/httpx/httpx_*.*.*_linux_arm64.zip -d $PWD/tools/httpx_arm64                                                             
-        rm -rf $PWD/tools/httpx/httpx_*.*.*_linux_arm64.zip                                                                                
-    fi                                                                                                                                     
+function install_httpx_arm64() {
+    if [ ! -f "$PWD/httpx" ]; then
+        curl https://api.github.com/repos/projectdiscovery/httpx/releases \
+		| grep "browser_download_url.*_linux_arm64.zip" \
+		| head -n1 \
+		| cut -d: -f2,3 \
+		| sed 's/"//g' \
+		| wget -qi - -P $PWD/tools/arm64/httpx
+        unzip $PWD/tools/arm64/httpx/httpx_*.*.*_linux_arm64.zip -d $PWD/tools/arm64/httpx
+        rm -rf $PWD/tools/arm64/httpx/httpx_*.*.*_linux_arm64.zip
+    fi
 }
 
 function install_katana() {
@@ -101,22 +97,22 @@ function install_katana() {
 		| head -n1 \
 		| cut -d: -f2,3 \
 		| sed 's/"//g' \
-		| wget -qi - -P $PWD/tools/katana
-        unzip $PWD/tools/katana/katana_*.*.*_linux_amd64.zip -d $PWD/tools/katana
-        rm -rf $PWD/tools/katana/katana_*.*.*_linux_amd64.zip
+		| wget -qi - -P $PWD/tools/amd64/katana
+        unzip $PWD/tools/amd64/katana/katana_*.*.*_linux_amd64.zip -d $PWD/tools/amd64/katana
+        rm -rf $PWD/tools/amd64/katana/katana_*.*.*_linux_amd64.zip
     fi
 }
 
 function install_katana_arm64() {
     if [ ! -f "$PWD/katana" ]; then
         curl https://api.github.com/repos/projectdiscovery/katana/releases \
-                | grep "browser_download_url.*_linux_arm64.zip" \
-                | head -n1 \
-                | cut -d: -f2,3 \
-                | sed 's/"//g' \
-                | wget -qi - -P $PWD/tools/katana
-        unzip $PWD/tools/katana/katana_*.*.*_linux_arm64.zip -d $PWD/tools/katana_arm64
-        rm -rf $PWD/tools/katana/katana_*.*.*_linux_arm64.zip
+		| grep "browser_download_url.*_linux_arm64.zip" \
+		| head -n1 \
+		| cut -d: -f2,3 \
+		| sed 's/"//g' \
+		| wget -qi - -P $PWD/tools/arm64/katana
+        unzip $PWD/tools/arm64/katana/katana_*.*.*_linux_arm64.zip -d $PWD/tools/arm64/katana
+        rm -rf $PWD/tools/arm64/katana/katana_*.*.*_linux_arm64.zip
     fi
 }
 
@@ -127,13 +123,28 @@ function install_nuclei_templates() {
     find $PWD/nuclei-templates -type f -name *.yaml | xargs -I % cp -rfv % $PWD/nuclei-templates/all 2>/dev/null
 }
 
-if [ $# -lt 2 ]; then
-    echo "OPTIONS:
+function usage() {
+    if [ $# -lt 2 ]; then
+        echo "OPTIONS:
 arm64 | amd64
 USAGE:
 ./install.sh <arg>
 "
-exit 1
+        exit 1
+    fi
+}
+
+function clean_up() {
+    rm -rf $PWD/tools/amd64/*
+}
+
+function clean_up_arm64() {
+    rm -rf $PWD/tools/arm64/*
+}
+
+if [ $# -lt 1 ] || [ $# -gt 1 ]; then
+    usage
+    exit 1
 fi
 
 if [ $1 == 'amd64' ]; then
@@ -143,13 +154,13 @@ if [ $1 == 'amd64' ]; then
     install_httpx
     install_katana
     install_nuclei_templates
-fi
-
-if [ $1 == 'arm64' ]; then
-    clean_up
+elif [ $1 == 'arm64' ]; then
+    clean_up_arm64
     install_aquatone_arm64
     install_nuclei_arm64
     install_httpx_arm64
     install_katana_arm64
     install_nuclei_templates
+else
+    usage
 fi
