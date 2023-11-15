0e780725355cf4eb82de8881fc82cfb8a96ebc49
War Torrente I <42901134+catx0rr@users.noreply.github.com>
2023-11-15 16:13:05 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+function clean_up() {
+    rm -rf $PWD/tools $PWD/nuclei-templates
+}
+
+function install_aquatone() {
+    if [ ! -f "$PWD/aquatone" ]; then
+	curl https://api.github.com/repos/michenriksen/aquatone/releases \
+		| grep "browser_download_url.*_linux_amd64_*.*.*zip" \
+		| head -n1 \
+		| cut -d: -f2,3 \
+		| tr -d '"' \
+		| wget -qi - -P $PWD/tools/aquatone
+
+        unzip $PWD/tools/aquatone/aquatone_linux_amd64_*.*.*.zip -d $PWD/tools/aquatone
+        rm -rf $PWD/tools/aquatone/aquatone_linux_amd64_*.*.*.zip
+    fi
+    
+    if [[ `dpkg-query -l chromium | grep ii | awk '{print $2}'` != "chromium" ]]; then
+        apt-get install -y chromium
+    fi
+}
+
+function install_nuclei() {
+    if [ ! -f "$PWD/nuclei" ]; then
+	curl https://api.github.com/repos/projectdiscovery/nuclei/releases \
+		| grep "browser_download_url.*_linux_amd64.zip" \
+		| head -n1 \
+		| cut -d: -f2,3 \
+		| tr -d '"' \
+		| wget -qi - -P $PWD/tools/nuclei
+
+        unzip $PWD/tools/nuclei/nuclei_*.*.*_linux_amd64.zip -d $PWD/tools/nuclei/
+        rm -rf $PWD/tools/nuclei/nuclei_*.*.*_linux_amd64.zip
+    fi
+}
+
+function install_httpx() {
+    if [ ! -f "$PWD/httpx" ]; then
+        curl https://api.github.com/repos/projectdiscovery/httpx/releases \
+		| grep "browser_download_url.*_linux_amd64.zip" \
+		| head -n1 \
+		| cut -d: -f2,3 \
+		| sed 's/"//g' \
+		| wget -qi - -P $PWD/tools/httpx
+        unzip $PWD/tools/httpx/httpx_*.*.*_linux_amd64.zip -d $PWD/tools/httpx
+        rm -rf $PWD/tools/httpx/httpx_*.*.*_linux_amd64.zip
+    fi
+}
+
+function install_katana() {
+    if [ ! -f "$PWD/katana" ]; then
+        curl https://api.github.com/repos/projectdiscovery/katana/releases \
+		| grep "browser_download_url.*_linux_amd64.zip" \
+		| head -n1 \
+		| cut -d: -f2,3 \
+		| sed 's/"//g' \
+		| wget -qi - -P $PWD/tools/katana
+        unzip $PWD/tools/katana/katana_*.*.*_linux_amd64.zip -d $PWD/tools/katana
+        rm -rf $PWD/tools/katana/katana_*.*.*_linux_amd64.zip
+    fi
+}
+
+function install_nuclei_templates() {
+    rm -rf $PWD/nuclei-templates 2>/dev/null
+    git clone https://github.com/projectdiscovery/nuclei-templates $PWD/nuclei-templates/
+    mkdir -p $PWD/nuclei-templates/all 2>/dev/null
+    find $PWD/nuclei-templates -type f -name *.yaml | xargs -I % cp -rfv % $PWD/nuclei-templates/all 2>/dev/null
+}
+
+clean_up
+install_aquatone
+install_nuclei
+install_httpx
+install_katana
+install_nuclei_templates
